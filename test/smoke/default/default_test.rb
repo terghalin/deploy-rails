d99a14773562594d6b3fe209c66a7d2736ee2d76
Aleksandr Tcitlionok <Aleksandr_Tcitlionok@epam.com>
2017-07-14 18:24:13 +0300

    Add initial tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,18 +1,9 @@
-# # encoding: utf-8
-
-# Inspec test for recipe deploy-rails::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# Application group
+describe group('railsapp') do
+  it { should exist }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+# Application user
+describe user('railsapp') do
+  it { should exist }
 end
