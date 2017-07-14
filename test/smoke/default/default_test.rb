cfbd3cc6e9fc42e5ca3543bcdd04d42ade5b91a6
Aleksandr Tcitlionok <Aleksandr_Tcitlionok@epam.com>
2017-07-14 18:42:52 +0300

    InSpec: describe gpg key import

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -9,3 +9,9 @@ describe user('railsapp') do
   its('group') { should eq 'railsapp' }
   its('home') { should eq '/home/railsapp' }
 end
+
+# Check if GPG-key import working
+describe command('gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+') do
+  its('exit_status') { should eq 0 }
+end
