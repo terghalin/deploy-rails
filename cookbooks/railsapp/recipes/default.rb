df99dd3005c994d2be7c016630793e14b72047c6
terghalin <terghalin@gmail.com>
2017-07-18 13:02:05 +0300

    Added database include for testing purpose

--- cookbooks/railsapp/recipes/default.rb
+++ cookbooks/railsapp/recipes/default.rb
@@ -1,4 +1,7 @@
 # Install required packages
-package %w(postgresql postgresql-contrib git-core curl libpq-dev zlib1g-dev build-essential imagemagick libffi-dev python-software-properties python-psycopg2) do
+package %w(git-core curl libpq-dev zlib1g-dev build-essential imagemagick libffi-dev python-software-properties python-psycopg2) do
   action :install
 end
+
+# TODO: decouple for another role
+include_recipe 'railsapp::database'
