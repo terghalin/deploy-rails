0f41d1c6726c3fe1439c7f1644238e35264b536b
terghalin <terghalin@gmail.com>
2017-07-18 13:01:37 +0300

    Add database and user creation on pgsql instance

--- cookbooks/railsapp/recipes/database.rb
+++ cookbooks/railsapp/recipes/database.rb
@@ -1,1 +1,17 @@
-include_recipe 'railsapp::database'
+# Create database user with password
+execute 'create_database_user' do
+  user 'postgres'
+  timeout 15
+  command "psql -c \"CREATE USER #{node['database']['user']['name']} WITH PASSWORD '#{node['database']['user']['password']}';\""
+
+  not_if "psql -t -c '\\du' | cut -d \\| -f 1 | grep -qw #{node['database']['user']['name']}", :user => 'postgres', :timeout => 15
+end
+
+# Create database and assign user as owner
+execute 'create_database' do
+  user 'postgres'
+  timeout 15
+  command "psql -c 'CREATE DATABASE #{node['database']['name']} OWNER #{node['database']['user']['name']};'"
+
+  not_if "psql -lqt | cut -d \\| -f 1 | grep -qw #{node['database']['name']}", :user => 'postgres', :timeout => 15
+end
